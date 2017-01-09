323b969163eee2ef7f40bf8230739d5ead4315ad
0xJCG <jonathancastrogonzalez@gmail.com>
2017-01-09 19:33:55 +0100

    Change the url validation and prepare sql functions

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
+import sqlite3
+
 from flask import Flask, request, g, jsonify
 from urlparse import urlparse
+from tld import get_tld
 
 app = Flask('genuine-url-checker')
 
 
-def is_valid_url(url):
-    import re
-    regex = re.compile(
-        r'^https?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return url is not None and regex.match(url) is not None
+DATABASE = '/db/database.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('db/schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def query_db(args=(), one=False):
+    query = "SELECT company FROM checked_domains WHERE domain = ? AND tld = ?"
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,13 +48,18 @@ def index():
 @app.route('/check/', methods=['POST'])
 def check_url():
     url = request.form['url'].lower().encode('utf8')
-    if is_valid_url(url):
-        web = urlparse(url)
-        domain = web.netloc
+    url_object = get_tld(url, as_object=True, fail_silently=True)
+    if url_object is not None:
+        company = query_db([url_object.domain, url_object.suffix], True)
+        if company:
+            return jsonify(is_genuine=True, company=company, url=url)
+        else:
+            return jsonify(is_genuine=False, url=url)
     else:
         return jsonify(error="incorrect url syntax", url=url)
 
 
 if __name__ == '__main__':
     app.debug = True
+    init_db()
     app.run()
